b7f9eeffa8601db8b479676a101a2578d78c84b7
ataka <masayuki.ataka@gmail.com>
2017-09-09 14:51:25 +0900

    Return response of Message and Task

--- chatwork.go
+++ chatwork.go
@@ -1,6 +1,7 @@
 package chatwork
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -26,9 +27,9 @@ func NewChatwork(apiKey string) *Chatwork {
 
 type endpoint string
 
-func (c *Chatwork) post(endpoint endpoint, vs url.Values) {
-	body := strings.NewReader(vs.Encode())
-	request, requestError := http.NewRequest("POST", string(endpoint), body)
+func (c *Chatwork) post(endpoint endpoint, vs url.Values) *http.Response {
+	reqBody := strings.NewReader(vs.Encode())
+	request, requestError := http.NewRequest("POST", string(endpoint), reqBody)
 	if requestError != nil {
 		log.Fatal(requestError)
 	}
@@ -38,10 +39,16 @@ func (c *Chatwork) post(endpoint endpoint, vs url.Values) {
 
 	httpClient := new(http.Client)
 	res, error := httpClient.Do(request)
-	defer res.Body.Close()
 	if error != nil {
 		log.Fatal(error)
 	}
+	return res
+}
+
+func decodeBody(res *http.Response, out interface{}) error {
+	defer res.Body.Close()
+	decoder := json.NewDecoder(res.Body)
+	return decoder.Decode(out)
 }
 
 type Text string
@@ -50,6 +57,10 @@ type RoomId int64
 type Message struct {
 	roomId RoomId
 	body   Text
+}
+
+type NewMessageResponse struct {
+	MessageId string `json:"message_id"`
 }
 
 func NewMessage(roomId int64, body string) *Message {
@@ -63,21 +74,31 @@ func endpointFmt(format string, a ...interface{}) string {
 	return fmt.Sprintf(format, a)
 }
 
-func (c *Chatwork) CreateMessage(message *Message) {
+func (c *Chatwork) CreateMessage(message *Message) *NewMessageResponse {
 	endpoint := endpoint(baseURL + fmt.Sprintf("rooms/%d/messages", message.roomId))
 	vs := url.Values{}
 	vs.Add("body", string(message.body))
-	c.post(endpoint, vs)
+	res := c.post(endpoint, vs)
+
+	var newMessageResponse NewMessageResponse
+	if err := decodeBody(res, &newMessageResponse); err != nil {
+		log.Fatal(err)
+	}
+	return &newMessageResponse
 }
 
 type UserId int64
 type UserIds []UserId
 
 type Task struct {
-	roomId     RoomId
-	body       Text
-	assignees  UserIds
+	roomId    RoomId
+	body      Text
+	assignees UserIds
 	due       *time.Time
+}
+
+type NewTaskResponse struct {
+	TaskIds []int64 `json:"task_ids"`
 }
 
 func NewTask(roomId int64, body string, assignees []int64, due *time.Time) *Task {
@@ -92,7 +113,7 @@ func NewTask(roomId int64, body string, assignees []int64, due *time.Time) *Task
 	return t
 }
 
-func (c *Chatwork) CreateTask(task *Task) {
+func (c *Chatwork) CreateTask(task *Task) *NewTaskResponse {
 	endpoint := endpoint(baseURL + fmt.Sprintf("rooms/%d/tasks", task.roomId))
 	vs := url.Values{}
 	vs.Add("body", string(task.body))
@@ -100,7 +121,13 @@ func (c *Chatwork) CreateTask(task *Task) {
 	if task.due != nil {
 		vs.Add("limit", strconv.FormatInt(task.due.Unix(), 10))
 	}
-	c.post(endpoint, vs)
+	res := c.post(endpoint, vs)
+
+	var newTaskResponse NewTaskResponse
+	if err := decodeBody(res, &newTaskResponse); err != nil {
+		log.Fatal(err)
+	}
+	return &newTaskResponse
 }
 
 func (ids UserIds) toString(sep string) string {
